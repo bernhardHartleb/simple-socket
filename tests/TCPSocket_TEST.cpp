2f290504a6d3ad45e8ca576cc9b97ec0a7e4c30d
Bernhard Hartleb <bernhard.hartleb@gmail.com>
2009-09-28 22:05:16 +0200

    extend testcase

--- tests/TCPSocket_TEST.cpp
+++ tests/TCPSocket_TEST.cpp
@@ -22,11 +22,16 @@ int main()
 
 	client_socket.connect( "127.0.0.1", 47777);
 
-	const TCPSocket::Handle& tmp_handle = server_socket.accept();
+	// Test socket handle
+	TCPSocket::Handle tmp_handle = server_socket.accept();
 	TCPSocket::Handle handle;
+	if(handle) return 1;
+	if(!tmp_handle) return 1;
 	handle = tmp_handle;
 	if(!handle) return 1;
+	if(tmp_handle) return 1;
 	TCPSocket session_socket(handle);
+	if(handle) return 1;
 	if( session_socket.peerDisconnected()) return 1;
 
 	session_socket.send( send_msg, len);
