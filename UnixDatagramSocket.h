5968bdb3c3472e5159a61771240166661ea77620
Andreas Wallner <Andreas.Wallner.etm07@fh-joanneum.at>
2009-09-30 21:58:19 +0200

    [doc] little fix in UnixDatagramSocket documentation

--- UnixDatagramSocket.h
+++ UnixDatagramSocket.h
@@ -10,39 +10,37 @@ namespace NET
 	{
 	public:
 		/*!
-			Construct a Unix datagram socket
-			\exception SocketException thrown if unable to create the socket
+		 * Construct a Unix datagram socket
+		 * \exception SocketException thrown if unable to create the socket
 		*/
 		UnixDatagramSocket();
 
 		/*!
-			Construct a UDP socket with the given local port and address
-			\param localPath parth to bind
-			\exception SocketException thrown if unable to create the socket
+		 * Construct a UDP socket with the given local port and address
+		 * \param localPath parth to bind
+		 * \exception SocketException thrown if unable to create the socket
 		*/
 		UnixDatagramSocket( const std::string &localPath);
 
 		/*!
-			Send the given buffer as a UDP datagram to the
-			specified address / port
-			\param buffer buffer to be written
-			\param bufferLen number of bytes to write
-			\param foreignAddress address (IP address or name) to send to
-			\param foreignPort port number to send to
-			\return true if send is successful
-			\exception SocketException thrown if unable to send datagram
+		 * Send the given buffer as a UDP datagram to the
+		 * specified address / port
+		 * \param buffer buffer to be written
+		 * \param len number of bytes to write
+		 * \param foreignPath filename of the datagram socket the data should be sent to
+		 * \return true if send is successful
+		 * \exception SocketException thrown if unable to send datagram
 		*/
 		void sendTo( const void* buffer, size_t len, const std::string& foreignPath);
 
 		/*!
-			Read read up to bufferLen bytes data from this socket.  The given buffer
-			is where the data will be placed
-			\param buffer buffer to receive data
-			\param bufferLen maximum number of bytes to receive
-			\param sourceAddress address of datagram source
-			\param sourcePort port of data source
-			\return number of bytes received and -1 for error
-			\exception SocketException thrown if unable to receive datagram
+		 * Read read up to bufferLen bytes data from this socket.  The given buffer
+		 * is where the data will be placed
+		 * \param buffer buffer to receive data
+		 * \param len maximum number of bytes to receive
+		 * \param sourcePath path where the data originated
+		 * \return number of bytes received and -1 for error
+		 * \exception SocketException thrown if unable to receive datagram
 		*/
 		int receiveFrom( void* buffer, size_t len, std::string& sourcePath);
 	};
