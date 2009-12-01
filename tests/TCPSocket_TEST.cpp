135a0711422dd2ce60938dbc74bbda6c724cc2b8
Bernhard Hartleb <bernhard.hartleb@gmail.com>
2009-12-01 17:46:46 +0100

    tests: rewrite TCPSocket for cppunit

--- tests/TCPSocket_TEST.cpp
+++ tests/TCPSocket_TEST.cpp
@@ -1,54 +1,81 @@
+#include <cppunit/extensions/HelperMacros.h>
 #include <TCPSocket.h>
 
 #include <cstring>
-#include <iostream>
 
-using namespace NET;
+static const char send_msg[] = "The quick brown fox jumps over the lazy dog";
+static char recv_msg[sizeof(send_msg)];
+static const int len = sizeof(send_msg);
 
-int main()
+class TCPSocketTest : public CppUnit::TestFixture
 {
-	static char send_msg[] = "The quick brown fox jumps over the lazy dog";
-	static char recv_msg[sizeof(send_msg)];
-	static size_t len = sizeof(send_msg);
+	CPPUNIT_TEST_SUITE( TCPSocketTest );
+	CPPUNIT_TEST( testSocketHandle );
+	CPPUNIT_TEST( testPeerStatus );
+	CPPUNIT_TEST_SUITE_END();
 
-	try {
+private:
+	NET::TCPSocket* server_socket;
+	NET::TCPSocket* client_socket;
 
-	// Test peer status on a TCP socket
-	TCPSocket server_socket;
-	TCPSocket client_socket;
-	server_socket.bind( "127.0.0.1", 47777);
-	server_socket.listen();
-	if( server_socket.peerDisconnected()) return 1;
-
-	client_socket.connect( "127.0.0.1", 47777);
-
-	// Test socket handle
-	TCPSocket::Handle tmp_handle = server_socket.accept();
-	TCPSocket::Handle handle;
-	TCPSocket::Handle& ref_handle = handle;
-	handle = ref_handle;
-	if(handle) return 1;
-	if(!tmp_handle) return 1;
-	handle = tmp_handle;
-	if(!handle) return 1;
-	if(tmp_handle) return 1;
-	TCPSocket session_socket(handle);
-	if(handle) return 1;
-	if( session_socket.peerDisconnected()) return 1;
-
-	session_socket.send( send_msg, len);
-	if( client_socket.receive( recv_msg, len) != len) return 1;
-	if( std::memcmp( send_msg, recv_msg, len) < 0) return 1;
-
-	client_socket.disconnect();
-	session_socket.send( send_msg, len);
-	if( !session_socket.peerDisconnected()) return 1;
-
-	// no errors in this testcase
-	} catch( const SocketException& e)
+public:
+	void setUp()
 	{
-		std::cerr << e.what() << std::endl;
-		std::cerr << e.errorCode() << std::endl;
-		return 1;
+		server_socket = new NET::TCPSocket();
+		client_socket = new NET::TCPSocket();
 	}
-}
+
+	void tearDown()
+	{
+		delete server_socket;
+		delete client_socket;
+	}
+
+	void testSocketHandle()
+	{
+		server_socket->bind( "127.0.0.1", 47777);
+		server_socket->listen();
+		client_socket->connect( "127.0.0.1", 47777);
+
+		NET::TCPSocket::Handle tmp_handle = server_socket->accept();
+		NET::TCPSocket::Handle handle;
+		NET::TCPSocket::Handle& ref_handle = handle;
+		handle = ref_handle;
+		CPPUNIT_ASSERT( !handle );
+		CPPUNIT_ASSERT( tmp_handle );
+		handle = tmp_handle;
+		CPPUNIT_ASSERT( handle );
+		CPPUNIT_ASSERT( !tmp_handle );
+		NET::TCPSocket session_socket(handle);
+		CPPUNIT_ASSERT( !handle );
+		CPPUNIT_ASSERT_THROW( NET::TCPSocket bad_socket(handle), NET::SocketException );
+		client_socket->disconnect();
+	}
+
+	void testPeerStatus()
+	{
+		int ret;
+		server_socket->bind( "127.0.0.1", 47777);
+		server_socket->listen();
+		CPPUNIT_ASSERT( !server_socket->peerDisconnected() );
+
+		client_socket->connect( "127.0.0.1", 47777);
+		NET::TCPSocket::Handle handle = server_socket->accept();
+		CPPUNIT_ASSERT( handle );
+		NET::TCPSocket session_socket(handle);
+		CPPUNIT_ASSERT( !handle );
+
+		session_socket.send( send_msg, len);
+		ret = client_socket->receive( recv_msg, len);
+		CPPUNIT_ASSERT_EQUAL( len, ret );
+		CPPUNIT_ASSERT( !session_socket.peerDisconnected() );
+		CPPUNIT_ASSERT( std::memcmp( send_msg, recv_msg, len) == 0 );
+
+		client_socket->disconnect();
+		ret = session_socket.send( send_msg, len);
+		CPPUNIT_ASSERT_EQUAL( -1, ret );
+		CPPUNIT_ASSERT( session_socket.peerDisconnected() );
+	}
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION( TCPSocketTest );
